97a0ce539d980caebf9935bc53c10d6821c66626
Briang15 <garcia.brianic3@gmail.com>
2019-07-11 09:32:41 -0400

    Adding strings.js

---
+++ strings.js
@@ -0,0 +1,11 @@
+// //let str= "";
+// for(let i=0; i < 4; i++){
+//     str +="*";
+//     console.log(str);
+// }
+
+let str= "";
+for(let i=0; i < 5000000; i++){
+    str +="brian";
+    console.log(str);
+}
